794bf7a8acb008c1f0be2759a8731a1022811380
ali <mustafaevalibek52@gmail.com>
2023-12-06 15:56:34 +0500

    some

--- script.js
+++ script.js
@@ -289,305 +289,307 @@
 
 
 //                                LESSON 4 HOMEWORK
-
-// Раскидать людей в разные массивы в зависимости от их почты
-// let users = [
-//     {
-//         "id": 1,
-//         "name": "Leanne Graham",
-//         "username": "Bret",
-//         "email": "Sincere@april.biz",
-//         "address": {
-//             "street": "Kulas Light",
-//             "suite": "Apt. 556",
-//             "city": "Gwenborough",
-//             "zipcode": "92998-3874",
-//             "geo": {
-//                 "lat": "-37.3159",
-//                 "lng": "81.1496"
-//             }
-//         },
-//         "phone": "1-770-736-8031 x56442",
-//         "website": "hildegard.org",
-//         "company": {
-//             "name": "Romaguera-Crona",
-//             "catchPhrase": "Multi-layered client-server neural-net",
-//             "bs": "harness real-time e-markets"
-//         }
-//     },
-//     {
-//         "id": 2,
-//         "name": "Ervin Howell",
-//         "username": "Antonette",
-//         "email": "Shanna@melissa.tv",
-//         "address": {
-//             "street": "Victor Plains",
-//             "suite": "Suite 879",
-//             "city": "Wisokyburgh",
-//             "zipcode": "90566-7771",
-//             "geo": {
-//                 "lat": "-43.9509",
-//                 "lng": "-34.4618"
-//             }
-//         },
-//         "phone": "010-692-6593 x09125",
-//         "website": "anastasia.net",
-//         "company": {
-//             "name": "Deckow-Crist",
-//             "catchPhrase": "Proactive didactic contingency",
-//             "bs": "synergize scalable supply-chains"
-//         }
-//     },
-//     {
-//         "id": 3,
-//         "name": "Clementine Bauch",
-//         "username": "Samantha",
-//         "email": "Nathan@yesenia.net",
-//         "address": {
-//             "street": "Douglas Extension",
-//             "suite": "Suite 847",
-//             "city": "McKenziehaven",
-//             "zipcode": "59590-4157",
-//             "geo": {
-//                 "lat": "-68.6102",
-//                 "lng": "-47.0653"
-//             }
-//         },
-//         "phone": "1-463-123-4447",
-//         "website": "ramiro.info",
-//         "company": {
-//             "name": "Romaguera-Jacobson",
-//             "catchPhrase": "Face to face bifurcated interface",
-//             "bs": "e-enable strategic applications"
-//         }
-//     },
-//     {
-//         "id": 4,
-//         "name": "Patricia Lebsack",
-//         "username": "Karianne",
-//         "email": "Julianne.OConner@kory.org",
-//         "address": {
-//             "street": "Hoeger Mall",
-//             "suite": "Apt. 692",
-//             "city": "South Elvis",
-//             "zipcode": "53919-4257",
-//             "geo": {
-//                 "lat": "29.4572",
-//                 "lng": "-164.2990"
-//             }
-//         },
-//         "phone": "493-170-9623 x156",
-//         "website": "kale.biz",
-//         "company": {
-//             "name": "Robel-Corkery",
-//             "catchPhrase": "Multi-tiered zero tolerance productivity",
-//             "bs": "transition cutting-edge web services"
-//         }
-//     },
-//     {
-//         "id": 5,
-//         "name": "Chelsey Dietrich",
-//         "username": "Kamren",
-//         "email": "Lucio_Hettinger@annie.ca",
-//         "address": {
-//             "street": "Skiles Walks",
-//             "suite": "Suite 351",
-//             "city": "Roscoeview",
-//             "zipcode": "33263",
-//             "geo": {
-//                 "lat": "-31.8129",
-//                 "lng": "62.5342"
-//             }
-//         },
-//         "phone": "(254)954-1289",
-//         "website": "demarco.info",
-//         "company": {
-//             "name": "Keebler LLC",
-//             "catchPhrase": "User-centric fault-tolerant solution",
-//             "bs": "revolutionize end-to-end systems"
-//         }
-//     },
-//     {
-//         "id": 6,
-//         "name": "Mrs. Dennis Schulist",
-//         "username": "Leopoldo_Corkery",
-//         "email": "Karley_Dach@jasper.info",
-//         "address": {
-//             "street": "Norberto Crossing",
-//             "suite": "Apt. 950",
-//             "city": "South Christy",
-//             "zipcode": "23505-1337",
-//             "geo": {
-//                 "lat": "-71.4197",
-//                 "lng": "71.7478"
-//             }
-//         },
-//         "phone": "1-477-935-8478 x6430",
-//         "website": "ola.org",
-//         "company": {
-//             "name": "Considine-Lockman",
-//             "catchPhrase": "Synchronised bottom-line interface",
-//             "bs": "e-enable innovative applications"
-//         }
-//     },
-//     {
-//         "id": 7,
-//         "name": "Kurtis Weissnat",
-//         "username": "Elwyn.Skiles",
-//         "email": "Telly.Hoeger@billy.biz",
-//         "address": {
-//             "street": "Rex Trail",
-//             "suite": "Suite 280",
-//             "city": "Howemouth",
-//             "zipcode": "58804-1099",
-//             "geo": {
-//                 "lat": "24.8918",
-//                 "lng": "21.8984"
-//             }
-//         },
-//         "phone": "210.067.6132",
-//         "website": "elvis.io",
-//         "company": {
-//             "name": "Johns Group",
-//             "catchPhrase": "Configurable multimedia task-force",
-//             "bs": "generate enterprise e-tailers"
-//         }
-//     },
-//     {
-//         "id": 8,
-//         "name": "Nicholas Runolfsdottir V",
-//         "username": "Maxime_Nienow",
-//         "email": "Sherwood@rosamond.me",
-//         "address": {
-//             "street": "Ellsworth Summit",
-//             "suite": "Suite 729",
-//             "city": "Aliyaview",
-//             "zipcode": "45169",
-//             "geo": {
-//                 "lat": "-14.3990",
-//                 "lng": "-120.7677"
-//             }
-//         },
-//         "phone": "586.493.6943 x140",
-//         "website": "jacynthe.com",
-//         "company": {
-//             "name": "Abernathy Group",
-//             "catchPhrase": "Implemented secondary concept",
-//             "bs": "e-enable extensible e-tailers"
-//         }
-//     },
-//     {
-//         "id": 9,
-//         "name": "Glenna Reichert",
-//         "username": "Delphine",
-//         "email": "Chaim_McDermott@dana.io",
-//         "address": {
-//             "street": "Dayna Park",
-//             "suite": "Suite 449",
-//             "city": "Bartholomebury",
-//             "zipcode": "76495-3109",
-//             "geo": {
-//                 "lat": "24.6463",
-//                 "lng": "-168.8889"
-//             }
-//         },
-//         "phone": "(775)976-6794 x41206",
-//         "website": "conrad.com",
-//         "company": {
-//             "name": "Yost and Sons",
-//             "catchPhrase": "Switchable contextually-based project",
-//             "bs": "aggregate real-time technologies"
-//         }
-//     },
-//     {
-//         "id": 10,
-//         "name": "Clementina DuBuque",
-//         "username": "Moriah.Stanton",
-//         "email": "Rey.Padberg@karina.biz",
-//         "address": {
-//             "street": "Kattie Turnpike",
-//             "suite": "Suite 198",
-//             "city": "Lebsackbury",
-//             "zipcode": "31428-2261",
-//             "geo": {
-//                 "lat": "-38.2386",
-//                 "lng": "57.2232"
-//             }
-//         },
-//         "phone": "024-648-3804",
-//         "website": "ambrose.net",
-//         "company": {
-//             "name": "Hoeger LLC",
-//             "catchPhrase": "Centralized empowering task-force",
-//             "bs": "target end-to-end models"
-//         }
-//     }
-// ]
-// let emails = {
-//     org: [],
-//     net: [],
-//     info: []
-// }
-// let other = []
+let users = [
+    {
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+    },
+    {
+        "id": 2,
+        "name": "Ervin Howell",
+        "username": "Antonette",
+        "email": "Shanna@melissa.tv",
+        "address": {
+            "street": "Victor Plains",
+            "suite": "Suite 879",
+            "city": "Wisokyburgh",
+            "zipcode": "90566-7771",
+            "geo": {
+                "lat": "-43.9509",
+                "lng": "-34.4618"
+            }
+        },
+        "phone": "010-692-6593 x09125",
+        "website": "anastasia.net",
+        "company": {
+            "name": "Deckow-Crist",
+            "catchPhrase": "Proactive didactic contingency",
+            "bs": "synergize scalable supply-chains"
+        }
+    },
+    {
+        "id": 3,
+        "name": "Clementine Bauch",
+        "username": "Samantha",
+        "email": "Nathan@yesenia.net",
+        "address": {
+            "street": "Douglas Extension",
+            "suite": "Suite 847",
+            "city": "McKenziehaven",
+            "zipcode": "59590-4157",
+            "geo": {
+                "lat": "-68.6102",
+                "lng": "-47.0653"
+            }
+        },
+        "phone": "1-463-123-4447",
+        "website": "ramiro.info",
+        "company": {
+            "name": "Romaguera-Jacobson",
+            "catchPhrase": "Face to face bifurcated interface",
+            "bs": "e-enable strategic applications"
+        }
+    },
+    {
+        "id": 4,
+        "name": "Patricia Lebsack",
+        "username": "Karianne",
+        "email": "Julianne.OConner@kory.org",
+        "address": {
+            "street": "Hoeger Mall",
+            "suite": "Apt. 692",
+            "city": "South Elvis",
+            "zipcode": "53919-4257",
+            "geo": {
+                "lat": "29.4572",
+                "lng": "-164.2990"
+            }
+        },
+        "phone": "493-170-9623 x156",
+        "website": "kale.biz",
+        "company": {
+            "name": "Robel-Corkery",
+            "catchPhrase": "Multi-tiered zero tolerance productivity",
+            "bs": "transition cutting-edge web services"
+        }
+    },
+    {
+        "id": 5,
+        "name": "Chelsey Dietrich",
+        "username": "Kamren",
+        "email": "Lucio_Hettinger@annie.ca",
+        "address": {
+            "street": "Skiles Walks",
+            "suite": "Suite 351",
+            "city": "Roscoeview",
+            "zipcode": "33263",
+            "geo": {
+                "lat": "-31.8129",
+                "lng": "62.5342"
+            }
+        },
+        "phone": "(254)954-1289",
+        "website": "demarco.info",
+        "company": {
+            "name": "Keebler LLC",
+            "catchPhrase": "User-centric fault-tolerant solution",
+            "bs": "revolutionize end-to-end systems"
+        }
+    },
+    {
+        "id": 6,
+        "name": "Mrs. Dennis Schulist",
+        "username": "Leopoldo_Corkery",
+        "email": "Karley_Dach@jasper.info",
+        "address": {
+            "street": "Norberto Crossing",
+            "suite": "Apt. 950",
+            "city": "South Christy",
+            "zipcode": "23505-1337",
+            "geo": {
+                "lat": "-71.4197",
+                "lng": "71.7478"
+            }
+        },
+        "phone": "1-477-935-8478 x6430",
+        "website": "ola.org",
+        "company": {
+            "name": "Considine-Lockman",
+            "catchPhrase": "Synchronised bottom-line interface",
+            "bs": "e-enable innovative applications"
+        }
+    },
+    {
+        "id": 7,
+        "name": "Kurtis Weissnat",
+        "username": "Elwyn.Skiles",
+        "email": "Telly.Hoeger@billy.biz",
+        "address": {
+            "street": "Rex Trail",
+            "suite": "Suite 280",
+            "city": "Howemouth",
+            "zipcode": "58804-1099",
+            "geo": {
+                "lat": "24.8918",
+                "lng": "21.8984"
+            }
+        },
+        "phone": "210.067.6132",
+        "website": "elvis.io",
+        "company": {
+            "name": "Johns Group",
+            "catchPhrase": "Configurable multimedia task-force",
+            "bs": "generate enterprise e-tailers"
+        }
+    },
+    {
+        "id": 8,
+        "name": "Nicholas Runolfsdottir V",
+        "username": "Maxime_Nienow",
+        "email": "Sherwood@rosamond.me",
+        "address": {
+            "street": "Ellsworth Summit",
+            "suite": "Suite 729",
+            "city": "Aliyaview",
+            "zipcode": "45169",
+            "geo": {
+                "lat": "-14.3990",
+                "lng": "-120.7677"
+            }
+        },
+        "phone": "586.493.6943 x140",
+        "website": "jacynthe.com",
+        "company": {
+            "name": "Abernathy Group",
+            "catchPhrase": "Implemented secondary concept",
+            "bs": "e-enable extensible e-tailers"
+        }
+    },
+    {
+        "id": 9,
+        "name": "Glenna Reichert",
+        "username": "Delphine",
+        "email": "Chaim_McDermott@dana.io",
+        "address": {
+            "street": "Dayna Park",
+            "suite": "Suite 449",
+            "city": "Bartholomebury",
+            "zipcode": "76495-3109",
+            "geo": {
+                "lat": "24.6463",
+                "lng": "-168.8889"
+            }
+        },
+        "phone": "(775)976-6794 x41206",
+        "website": "conrad.com",
+        "company": {
+            "name": "Yost and Sons",
+            "catchPhrase": "Switchable contextually-based project",
+            "bs": "aggregate real-time technologies"
+        }
+    },
+    {
+        "id": 10,
+        "name": "Clementina DuBuque",
+        "username": "Moriah.Stanton",
+        "email": "Rey.Padberg@karina.biz",
+        "address": {
+            "street": "Kattie Turnpike",
+            "suite": "Suite 198",
+            "city": "Lebsackbury",
+            "zipcode": "31428-2261",
+            "geo": {
+                "lat": "-38.2386",
+                "lng": "57.2232"
+            }
+        },
+        "phone": "024-648-3804",
+        "website": "ambrose.net",
+        "company": {
+            "name": "Hoeger LLC",
+            "catchPhrase": "Centralized empowering task-force",
+            "bs": "target end-to-end models"
+        }
+    }
+]
+let emails = {
+    org: [],
+    net: [],
+    info: []
+}
+let other = []
 
 
-// users.filter(item => {
-//     let m = item.email.split('.').at(-1)
+users.filter(item => {
+    let m = item.email.split('.').at(-1)
 
-//     if (m === 'org') {
-//         emails.org.push(item.email)
-//     } else if (m === 'net') {
-//         emails.net.push(item.email)
-//     } else if (m === 'info') {
-//         emails.info.push(item.email)
-//     } else {
-//         other.push(item.email)
-//     }
-// })
+    if (emails[m]) {
+        emails[m].push(item)
+    } else {
+        other.push(item.email)
+    }
 
-// console.log("org:", "net:", "info:", emails);
-// console.log("Other emails:", other);
+    // if (m === 'org') {
+    //     emails.org.push(item.email)
+    // } else if (m === 'net') {
+    //     emails.net.push(item.email)
+    // } else if (m === 'info') {
+    //     emails.info.push(item.email)
+    // } else {
+    //     other.push(item.email)
+    // }
+})
+
+console.log("org:", "net:", "info:", emails);
+console.log("Other emails:", other);
 
 
 //                      LESSON 5
-// let Jaxongir = {
-//     personal: {
-//         name: "Jaxongir",
-//         surname: "Abdulaev",
-//         hands: 2,
-//         legs: 4,
-//         job: "Developer",
-//         city: {
-//             live: "Las Vegas",
-//             sport:{
-//                 hobby: "MMA",
-//             },
-//         },
-//         married: {
-//             wife: true,
-//             girlfriend: "Asalya"
-//         },
-//     },
-//     stydy: {
-//         school: 33,
-//         grade: 11,
-//         language: "English",
-//         head: true,
-//         abroad: {
-//             wantToStaudy: "U.S.A"
-//         },
-//     },
-//     age: 17,
-//     year: 2006,
-//     sex: "male",
-//     birth: "3 september",
-// }
+let Jaxongir = {
+    personal: {
+        name: "Jaxongir",
+        surname: "Abdulaev",
+        hands: 2,
+        legs: 4,
+        job: "Developer",
+        city: {
+            live: "Las Vegas",
+            sport: {
+                hobby: "MMA",
+            },
+        },
+        married: {
+            wife: true,
+            girlfriend: "Asalya"
+        },
+    },
+    stydy: {
+        school: 33,
+        grade: 11,
+        language: "English",
+        head: true,
+        abroad: {
+            wantToStaudy: "U.S.A"
+        },
+    },
+    age: 17,
+    year: 2006,
+    sex: "male",
+    birth: "3 september",
+}
 
 
 
-//                           LESSON 5 HOMEWORK
-// let ask = prompt('Which car do you want to buy?').toLowerCase().trim()
-
+//                      LESSON 5 HOMEWORK
 let cars = [{
     BMW: {
         compoany: "BMW",
@@ -671,11 +673,93 @@ let cars = [{
     },
 }]
 
-let ask = +prompt('Max price')
-let fil = cars.filter(item => {
-    if(item.price <= ask) {
-        console.log(item);
+let yearCar = 2023
+let carNew = []
+
+cars.forEach(item => {
+    if (yearCar - item.year <= 2) {
+        carNew.push(item)
     }
 })
 
-console.log(cars);
+let askOne = +prompt('Max price')
+let askTwo = +prompt('Min price')
+let a = []
+
+cars.forEach(item => {
+    if(item.price >= askOne && item.price <= askTwo) {
+        a.push(item)
+    }
+})
+
+console.log(carNew);
+console.log(a);
+
+
+//                           LESSON 6 JS
+// let car = {
+//     compoany: "BMW",
+//     model: "m5 competition",
+//     price: 200000,
+//     year: 2023,
+//     details: {
+//         driveUnit: "full",
+//         color: "dark blue",
+//         engineCapacity: "5",
+//         horsePower: "885 л.с",
+//         maxSpeed: "380 km/h",
+//         acceleration: "0-100: 2,3 second",
+//         supercharger: "Biturbo",
+//         country: "Germany",
+//     }
+// }
+
+
+// let person = {
+//     firstName: 'Ali',
+//     lastName: 'Aliev',
+//     age: 17,
+//     address: {
+//         street: 'Gagarin',
+//         city: 'Miami',
+//         zipCode: '12345'
+//     },
+//     contacts: [
+//         {
+//             type: 'email',
+//             value: 'aliev.ali@email.com'
+//         },
+//         {
+//             type: 'phone',
+//             value: '555-1234'
+//         }
+//     ]
+// };
+
+// let types = {
+//     string: [],
+//     number: [],
+//     object: [],
+//     boolean: []
+// }
+
+// let object = Object.assign({}, car, person);
+// let key = Object.keys(object);
+// let values = Object.values(key);
+// let arr = key.concat(values)
+
+// arr.filter(item => {
+//     if (typeof (item) === "string") {
+//         types.string.push(item)
+//     } else if (typeof (item) === "number") {
+//         types.number.push(item)
+//     } else if (typeof (item) === "object") {
+//         types.object.push(item)
+//     } else if (typeof (item) === "boolean") {
+//         types.boolean.push(item)
+//     }
+// })
+
+// console.log(arr);
+
+
